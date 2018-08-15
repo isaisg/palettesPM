6451d2e6690da2068bb4d217e174c70f631935d4
isai <isai.salas.gonzalez@gmail.com>
2018-08-15 12:56:55 -0400

    Add colour files

---
+++ R/colours.R
@@ -0,0 +1,12 @@
+
+#' Get the list of fractions samples acrros several experiments
+#' @return a vector of the canonical names of the fractions
+#' @references 
+#' @export
+
+pm_fractions <- function...) {
+	
+	fractions <- c("Inoculum,"BulkSoil,"Soil","Root","Shoot","Rhizosphere","EC","Sand","Agar")
+	fractions <- sort(fractions)
+	return(fractions)
+}
