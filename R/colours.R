5920f45f3ce9c3d959344dff4da3961fdb7f98c4
isai <isai.salas.gonzalez@gmail.com>
2018-11-06 21:57:52 -0500

    Change agar colors

--- R/colours.R
+++ R/colours.R
@@ -13,7 +13,7 @@ pm.names.fractions <- function(...) {
 #' @export
 pm.colors.fractions <- function(...) {
 	fractions <- pm.names.fractions()
-	cols <- c("#E8A6FF","#74537F","#DAA520","#7CFC00","#414141","#0000EE","#B27612","#7D17CC","#29AB5C","#7D17CC")
+	cols <- c("#FFC6CD","#E8A6FF","#DAA520","#7CFC00","#414141","#0000EE","#B27612","#7D17CC","#29AB5C","#7D17CC")
 	return( setNames(cols, fractions) )
 }
 
